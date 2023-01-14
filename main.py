a727d1b73a44d76c1858c02561acfa80eebbe7fb
celeste <parafactual@gmail.com>
2023-01-13 17:11:45 -0800

    lol

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == IAN:
+    if message.author.id == IAN:
         return
 
     if message.content.startswith('ada give '):
